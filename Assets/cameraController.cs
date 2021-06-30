1517de219813fd594a6d195ee809cda137641cd7
younessaa <y1o2u3n4@gmail.com>
2021-06-30 22:02:10 +0100

    update final

--- Assets/cameraController.cs
+++ Assets/cameraController.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-    public Transform[] views;
-    public float transitionSpeed;
-    Transform currentView;
+    public Transform[] views; // tableaux des différents views
+    public float transitionSpeed; // vitesse de transition
+    Transform currentView; // vue actuelle
 
     // Start is called before the first frame update
     void Start()
     {
+        // initialisation de currentView par la première vue
         currentView = views[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+        // si la button 1 taper appliquer la premiere vue
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             currentView = views[0];
         }
+        // si la button 2 taper appliquer la 2eme vue
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             currentView = views[1];
         }
+        // si la button 3 taper appliquer la 3eme vue
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             currentView = views[2];
@@ -33,8 +37,11 @@ public class cameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        //Lerp position
+        // transformation du la position de caméra
+        // Translation de caméra
         transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        // Rotation de caméra
         Vector3 currentAngle = new Vector3 (
            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
